0f9778532bdf13e12a6f1ea64780c76e7ed7b063
XeonPowder <xeonpowder@gmail.com>
2015-12-01 14:01:23 +0100

    Added first comments!
    comments

--- Source/Main.hx
+++ Source/Main.hx
@@ -4,6 +4,7 @@ import core3.Manager in Game;
 
 class Main{
 	public static function main(){
+		//variables
 		var pName:String = "NewPlayer";
 		var wString:String = "";
 		var hString:String = "";
@@ -11,31 +12,49 @@ class Main{
 		var arY:Int = 9;
 		var w:Int = 0;
 		var h:Int = 0;
+		// print String
 		Sys.print("Enter Player's name: ");
+		// read input to String
 		pName = Sys.stdin().readLine();
+		//output the read input (debug)
 		trace("Name: " + pName);
 		
 
-
+		// print String
 		Sys.print("Enter Tile Pixel Width: ");
+		// read input to String
 		wString = Sys.stdin().readLine();
+		//output the read input (debug)
 		trace("Name: " + wString);
+		//parse input from string to integer
 		arX = Std.parseInt(wString);
+		// print String		
 		Sys.print("Enter Tile Pixel Height: ");
+		// read input to String		
 		hString = Sys.stdin().readLine();
+		//output the read input (debug)		
 		trace("Name: " + hString);
+		//parse input from string to integer
 		arY = Std.parseInt(hString);
+		
 
-
-
+		// print String
 		Sys.print("Enter Game Pixel Width: ");
+		// read input to String
 		wString = Sys.stdin().readLine();
+		//output the read input (debug)
 		trace("Name: " + wString);
+		//parse input from string to integer
 		w = Std.parseInt(wString);
+		// print String
 		Sys.print("Enter Game Pixel Height: ");
+		// read input to String
 		hString = Sys.stdin().readLine();
+		//output the read input (debug)
 		trace("Name: " + hString);
+		//parse input from string to integer
 		h = Std.parseInt(hString);
+		
 		new Game(arX, arY, w, h, pName);
 	}
 }
